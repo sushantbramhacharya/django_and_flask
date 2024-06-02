3dd77808ca846949067eb90465d431cabf1d965c
sushantbramhacharya <sushantbramacharya@gmail.com>
2024-06-02 21:11:18 +0545

    Passing Value in Jinga

--- project/project/views.py
+++ project/project/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"index.html")
+    products = [
+        {"name": "Product 1", "price": 10.00},
+        {"name": "Product 2", "price": 20.00},
+        {"name": "Product 3", "price": 30.00},
+        {"name": "Product 4", "price": 40.00},
+        {"name": "Product 5", "price": 50.00},
+    ]
+    return render(request,"index.html",{'products':products})
