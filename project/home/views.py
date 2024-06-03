aaf026b61c6f9f8b6fa24768f9ee283b5e67e4d3
sushantbramhacharya <sushantbramacharya@gmail.com>
2024-06-03 21:34:01 +0545

    files template managed

--- project/home/views.py
+++ project/home/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello this is home app")
+    products = [
+        {"name": "Product 1", "price": 10.00},
+        {"name": "Product 2", "price": 20.00},
+        {"name": "Product 3", "price": 30.00},
+        {"name": "Product 4", "price": 40.00},
+        {"name": "Product 5", "price": 50.00},
+    ]
+    return render(request,'home.html',{'products':products})
 
 def products(request):
     return HttpResponse("This is products page")
