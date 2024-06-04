65167b50260f3b2a977188db7d5aae609db9e5bd
sushantbramhacharya <sushantbramacharya@gmail.com>
2024-06-04 17:26:15 +0545

    URL params

--- project/home/urls.py
+++ project/home/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('products/',views.products,name="products")
+    # path('',views.home,name="home"),
+    # path('products/',views.products,name="products"),
+
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polts/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- project/home/views.py
+++ project/home/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def home(request):
-    products = [
-        {"name": "Product 1", "price": 10.00},
-        {"name": "Product 2", "price": 20.00},
-        {"name": "Product 3", "price": 30.00},
-        {"name": "Product 4", "price": 40.00},
-        {"name": "Product 5", "price": 50.00},
-    ]
-    return render(request,'home.html',{'products':products})
 
-def products(request):
-    return HttpResponse("This is products page")
+# Create your views here.
+# def home(request):
+#     products = [
+#         {"name": "Product 1", "price": 10.00},
+#         {"name": "Product 2", "price": 20.00},
+#         {"name": "Product 3", "price": 30.00},
+#         {"name": "Product 4", "price": 40.00},
+#         {"name": "Product 5", "price": 50.00},
+#     ]
+#     return render(request,'home.html',{'products':products})
+
+# def products(request):
+#     return HttpResponse("This is products page")
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
