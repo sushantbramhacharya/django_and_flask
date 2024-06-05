eda92bbba4fe32f8d31079010bcc25b030028852
sushantbramhacharya <sushantbramacharya@gmail.com>
2024-06-05 21:21:41 +0545

    Flask Form Submission and Response

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask,request
 
 app=Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World'
+
+@app.route('/submit', methods=['GET'])
+def warn():
+    return "<h1>Please fill up the form.</h1>"
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    choice = request.form['choice']
+    return "<h1>The name of user is "+name+". And his/her choice is "+choice+".</h1>"
 
 if __name__ == '__main__':
     app.run()
