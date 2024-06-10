240792a7074b241137d47639f0424152a627ce17
sushantbramhacharya <sushantbramacharya@gmail.com>
2024-06-10 19:56:15 +0545

    Final Admin

--- project/home/admin.py
+++ project/home/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from . import models
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 1
+
 # Register your models here.
-admin.site.register(models.Question)
-admin.site.register(models.Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    # fields=["pub_date","question_text"]
+    fieldsets = [
+        ('Question info', {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines=[ChoiceInline]
+    list_display =['pub_date','question_text','was_published_recently']
+    list_editable=['question_text']
+    list_filter=['pub_date','question_text']
+    search_fields=['question_text']
+
+# Order of fields in Django admin models 
+# class ChoiceAdmin(admin.ModelAdmin):
+#     fields=['votes','question','choice_text']
+
+admin.site.register(models.Question,QuestionAdmin)
--- project/home/models.py
+++ project/home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 import datetime
 
 class Question(models.Model):
@@ -7,6 +8,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
